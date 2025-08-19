3499e8e5490625e343c347a005dd45b6af3720b2
viniciusLopes <44060641+viniciusLOCampos@users.noreply.github.com>
2025-08-19 10:50:57 -0300

    recreate_commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { UserForm } from "@/components/UserForm";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-primary/5 to-secondary/5">
+      <UserForm />
+    </div>
+  );
+};
+
+export default Index;
